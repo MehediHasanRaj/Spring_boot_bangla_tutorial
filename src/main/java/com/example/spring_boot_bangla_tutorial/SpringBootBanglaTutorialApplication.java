1af3d63e096ee5511fa27b8703c89e5dec708fb9
Raj <mehedihasanraj007@gmail.com>
2023-11-06 03:40:23 +0000

    Initial commit

---
+++ src/main/java/com/example/spring_boot_bangla_tutorial/SpringBootBanglaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_bangla_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBanglaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBanglaTutorialApplication.class, args);
+    }
+
+}
